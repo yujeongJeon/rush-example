2dc6f8a6d87385a7080004a559da19f009b67438
yujeong-jeon <yujeong.jeon@navercorp.com>
2022-02-17 18:49:40 +0900

    ✅ 패키지 수정 시, 의존하고 있는 패키지또한 영향 받는지 테스트

--- packages/axiosInstance/src/index.ts
+++ packages/axiosInstance/src/index.ts
@@ -4,9 +4,18 @@ import axios from "axios";
 
 const axiosInstance = axios.create()
 
+const CancelToken = axios.CancelToken
+export let source = CancelToken.source()
+
+export const cancelApi = () => {
+    source.cancel()
+    source = CancelToken.source()
+}
+
 export const api = async <Response = unknown, Request = unknown>(req: RequestConfig<Request>): Promise<Response> => {
   const res = await axiosInstance.request<Response, Response>({
       ...req,
+      cancelToken: source.token,
   })
 
   return res
